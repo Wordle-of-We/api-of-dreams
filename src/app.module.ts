b21e0778fdcfc830ac5b97e4debe40ce5423bc42
avelando <avelarrodrigues89@gmail.com>
2025-08-07 08:52:26 -0300

    novo commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CharactersModule } from './characters/characters.module';
@@ -10,17 +12,14 @@ import { PlaysModule } from './plays/plays.module';
 import { StatsModule } from './stats/stats.module';
 import { GameModeModule } from './game-mode/game-mode.module';
 import { AdminModule } from './admin/admin.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth/auth.service';
-import { JwtStrategy } from './auth/strategy/jwt.strategy';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
     PrismaModule,
     ScheduleModule.forRoot(),
-    AuthModule,
+
     UsersModule,
     CharactersModule,
     FranchisesModule,
@@ -29,17 +28,8 @@ import { AuthController } from './auth/auth.controller';
     StatsModule,
     GameModeModule,
     AdminModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (cs: ConfigService) => ({
-        secret: cs.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
-      }),
-      inject: [ConfigService],
-    }),
+
+    AuthModule,
   ],
-  providers: [AuthService, JwtStrategy],
-  controllers: [AuthController],
 })
-export class AppModule { }
+export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
-import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'supersecret',
-      signOptions: { expiresIn: '7d' },
+    ConfigModule,
+
+    UsersModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule, UsersModule],
+      inject: [ConfigService],
+      useFactory: (cs: ConfigService) => ({
+        secret: cs.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, PrismaService],
+  exports: [
+    AuthService,
+    JwtModule,
+  ],
 })
 export class AuthModule {}
