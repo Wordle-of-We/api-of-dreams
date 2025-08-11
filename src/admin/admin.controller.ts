de15f57975666cf91ec0701e90b8c1a5be3e0fa6
avelando <avelarrodrigues89@gmail.com>
2025-08-11 01:55:23 -0300

    fix(admin): corrigindo os kpis para admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,10 @@
-import {
-  Controller,
-  Get,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { KPI } from './dto/kpi.dto';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { RolesGuard } from '../auth/guard/roles.guard';
 import { Roles } from '../../common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('admin/dashboard')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -15,7 +12,7 @@ export class AdminController {
   constructor(private readonly svc: AdminService) {}
 
   @Get('kpis')
-  @Roles('ADMIN')
+  @Roles(Role.ADMIN)
   kpis(): Promise<KPI> {
     return this.svc.getKPIs();
   }
